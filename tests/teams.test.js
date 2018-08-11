ccbb42e341096e47f75c1c012d13f88c490f9513
James <jbkuczma@buffalo.edu>
2018-08-11 15:44:15 -0400

    update teams tests

--- tests/teams.test.js
+++ tests/teams.test.js
@@ -4,7 +4,7 @@ const BUFFALO_SABRES_TEAM_ID = 7
 
 describe('nhl.teams', () => {
   describe('allTeams()', () => {
-    it('should return 31 teams', (done) => {
+    it('should return 31 teams', done => {
       nhl.teams.allTeams().then(data => {
         if (data.teams.length === 31) {
           done()
@@ -16,12 +16,24 @@ describe('nhl.teams', () => {
     })
   })
   describe('team()', () => {
-    it('should return the Buffalo Sabres', (done) => {
+    it('should return the Buffalo Sabres', done => {
       nhl.teams.team(BUFFALO_SABRES_TEAM_ID).then(data => {
         if (data.teams.length === 1 && data.teams[0].name === 'Buffalo Sabres') {
           done()
         } else {
           throw new Error('The Buffalo Sabres were not returned')
+        }
+      })
+      .catch(error => done(error))
+    })
+  })
+  describe('roster()', () => {
+    it('response should contain a roster property', done => {
+      nhl.teams.roster(BUFFALO_SABRES_TEAM_ID).then(data => {
+        if (data.teams.length === 1 && data.teams[0].hasOwnProperty('roster')) {
+          done()
+        } else {
+          throw new Error('Roster was not found')
         }
       })
       .catch(error => done(error))
