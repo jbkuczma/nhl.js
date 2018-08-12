131fdeeff29005abbcb1402472548480ba7e4d21
James <jbkuczma@buffalo.edu>
2018-08-12 16:45:55 -0400

    player stats endpoint

--- src/api/players/constants/defaults.js
+++ src/api/players/constants/defaults.js
@@ -1,9 +1,15 @@
 const DEFAULTS = {
-
+  statType: 'statsSingleSeason'
 }
 
 export const PLAYER = {
   method: 'player',
   parameter: 'PLAYER_ID',
   endpoint: '/people',
+}
+
+export const STATS = {
+  method: 'stats',
+  parameter: 'playerID',
+  endpoint: '/people/{{playerID}}/stats'
 }
