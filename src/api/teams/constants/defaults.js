5ead980aa4c12b0aeb4c365e6775b6f63520cab4
James <jbkuczma@buffalo.edu>
2018-08-12 08:24:41 -0400

    remove old roster and stats implementation

--- src/api/teams/constants/defaults.js
+++ src/api/teams/constants/defaults.js
@@ -15,24 +15,12 @@ export const TEAM = {
 
 // https://statsapi.web.nhl.com/api/v1/teams/7/roster instead (?)
 export const TEAM_ROSTER = {
-  // expand: [
-  //   'team.roster'
-  // ],
-  // method: 'roster',
-  // parameter: 'TEAM_ID',
-  // endpoint: '/teams'
   method: 'roster',
   parameter: 'teamID',
   endpoint: `/teams/{{teamID}}/roster`
 }
 
 export const TEAM_STATS = {
-  // expand: [
-  //   'team.stats'
-  // ],
-  // method: 'stats',
-  // parameter: 'TEAM_ID',
-  // endpoint: '/teams'
   method: 'stats',
   parameter: 'teamID',
   endpoint: '/teams/{{teamID}}/stats'
