ac5c324d561cc1407bd4b161f2a888198cc1464a
James <jbkuczma@buffalo.edu>
2018-08-12 19:08:14 -0400

    update tests

--- tests/nhl.test.js
+++ tests/nhl.test.js
@@ -2,10 +2,23 @@
 
 const nhl = require('../lib')
 const should = require('should')
+const SUBMODULES = ['teams', 'players', 'league']
 
 describe('index', () => {
   it('should export `teams`, `players`, `league` submodules', done => {
-    nhl.should.have.properties(['teams', 'players', 'league'])
+    nhl.should.have.properties(SUBMODULES)
+    done()
+  })
+})
+
+describe('submodule index', () => {
+  it('should only export functions', done => {
+    Object.keys(nhl).forEach(key => {
+      if (key !== 'default') {
+        const obj = nhl[key]
+        Object.keys(obj).every(v => typeof obj[v] === 'function').should.be.true()
+      }
+    })
     done()
   })
 })
